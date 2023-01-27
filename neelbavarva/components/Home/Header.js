b1c0fc5faf3c0fbb16db69b32f1e37f49600395a
neel <neel@neel.local>
2023-01-27 19:22:42 +0530

    dynamic header completed

--- neelbavarva/components/Home/Header.js
+++ neelbavarva/components/Home/Header.js
@@ -1,8 +1,5 @@
-import { useEffect, useReducer, useState } from 'react'
-import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home/Header.module.css'
-import { FaLinkedinIn, FaGithub, FaTwitter } from 'react-icons/fa';
 import Link from 'next/link';
 import TwitterIcon from '../../public/icons/twitter.svg'
 import GitHubIcon from '../../public/icons/github.svg'
@@ -14,20 +11,20 @@ export default function Header() {
     return (
         <div className={`${styles.header} ${styles.container}`}>
             <div className={styles.header_title}>
-                Web and Android Developer creating thoughtful, intuitive interfaces.
+                Web and Android Developer creating Thoughtful, Intuitive Interfaces.
             </div>
             <div className={styles.header_content}>
                 <p>
                     Hi, I am Neel Bavarva, a final year electronics and 
                     communications engineering undergraduate pursuing my bachelor&apos;s 
-                    degree from NIT Surat, India. Know more <Link href="/Main/About">About Me</Link>.
+                    degree from NIT Surat, India.
                 </p>
                 
                 <p>
                     I am a self taught programmer. Through constant learning and creation, 
                     I produce aesthetic software to the highest standard.
                     I enjoy competing in hackathons and working on software projects.
-                    I also like to work on my problem solving skills on <a href="https://leetcode.com/neelbavarva/" target="_blank" rel="noreferrer">Leetcode</a> and <a href="https://www.codechef.com/users/neelbavarva/" target="_blank" rel="noreferrer">CodeChef</a>.
+                    I also like to work on my problem solving skills on <a href="https://leetcode.com/neelbavarva/" target="_blank" rel="noreferrer">Leetcode</a>.
 
                     Here&apos;s my <a href="https://drive.google.com/file/d/1oyi0gHekD9A7rUDXpGGzX2-RkawIIYFP/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>.
                 </p>
@@ -40,17 +37,17 @@ export default function Header() {
                             <Image className={styles.social_links_img} src={TwitterIcon} />
                         </div>
                     </a>
-                    <a href="https://twitter.com/neelbavarva">
+                    <a href="https://www.instagram.com/never_neel">
                         <div className={styles.social_links_container}>
                             <Image className={styles.social_links_img} src={InstagramIcon} />
                         </div>
                     </a>
-                    <a href="https://twitter.com/neelbavarva">
+                    <a href="https://dribbble.com/neelbavarva">
                         <div className={styles.social_links_container}>
                             <Image className={styles.social_links_img} src={DribbbleIcon} />
                         </div>
                     </a>
-                    <a href="https://twitter.com/neelbavarva">
+                    <a href="https://github.com/neelbavarva">
                         <div className={styles.social_links_container}>
                             <Image className={styles.social_links_img} src={GitHubIcon} />
                         </div>
@@ -58,7 +55,7 @@ export default function Header() {
                 </div>
                 <div>
                     <button>
-                        <a href="mailto:neelbavarva5@gmail.com">Email me</a>
+                        <a href="mailto:neelbavarva5@gmail.com">Email&nbsp;me</a>
                     </button>
                 </div>
             </div>
